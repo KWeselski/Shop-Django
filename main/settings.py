4fc4576a588561712efcdc76d6fef2892eb1e562
KWeselski <weselski.kamil@gmail.com>
2020-12-29 18:32:00 +0100

    change database

--- main/settings.py
+++ main/settings.py
@@ -92,15 +92,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
